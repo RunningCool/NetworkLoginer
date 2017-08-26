3ae556882ad5b86f28ad4521d35f0308b20c6d5a
Richard <richard.dtj@gmail.com>
2017-08-26 23:15:47 +0800

    Fixed the bug that cause false login success. Added error control on network failures and authentication failures.

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
-import requests, json, getpass, time
+import requests, json, getpass, time, sys
 
 auth_page_url='https://controller.shanghaitech.edu.cn:8445/PortalServer/customize/1478262836414/pc/auth.jsp'
 auth_url="https://controller.shanghaitech.edu.cn:8445/PortalServer/Webauth/webAuthAction!login.action"
-sleep_seconds=60
+retry_seconds=10
+heartbeat_seconds=60
+
+class LoginResult:
+    SUCCESS = 1
+    NETWORK_FAILURE = 2
+    AUTHENTICATION_FAILURE = 3
 
 def login(username, password):
     session = requests.Session()
-    session.get(auth_page_url)
+    try:
+        session.get(auth_page_url)
+    except Exception:
+        print 'Connection fails.'
+        return LoginResult.NETWORK_FAILURE
     post_data={
       "userName": username,
       "password": password,
-      "hasValidateCode": False,
-      "validCode": "",
-      "authLan": "zh_CN",
-      "hasValidateNextUpdatePassword": "true",
-      "rememberPwd": "false",
-      "browserFlag": "zh",
-      "hasCheckCode": False,
-      "checkcode": "",
-      "saveTime": "14",
-      "autoLogin": "false",
-      "userMac": "",
-      "isBoardPage": False,
-      "clientIp": "10.20.196.23"
+      "hasValidateCode": False
     }
-    response = session.post(auth_url, data=post_data)
+    try:
+        response = session.post(auth_url, data=post_data)
+    except Exception:
+        print 'Connection fails.'
+        return LoginResult.NETWORK_FAILURE
     json_ = json.loads(response.text)
     if json_['success'] == True:
         print 'Network authentication succeeeds.'
         print 'Client ip: ' + json_['data']['ip']
         print 'Login time: ' + json_['data']['loginDate']
-        return True
+        return LoginResult.SUCCESS
     else:
         print 'Network authentication fails.'
         print 'Remote reason: ' + json_['message']
-        return False
+        return LoginResult.AUTHENTICATION_FAILURE
 
 def human_login():
     username = raw_input('Plese input username: ')
@@ -42,11 +44,24 @@ def human_login():
     return username, password
 
 def controller():
-    username, password = human_login()
+    # print "This is the name of the script: ", sys.argv[0]
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        password = sys.argv[2]
+    elif len(sys.argv) == 1:
+        print 'Tips: run \'' + sys.argv[0] + ' username password\' next time to enable easy login.'
+        username, password = human_login()
+    else:
+        print 'Invalid number of arguments.'
     while True:
-        if login(username, password):
-            print 'Will check authentication again in ' + str(sleep_seconds) + ' seconds.'
-            time.sleep(sleep_seconds)
+        result = login(username, password)
+        if result == LoginResult.SUCCESS:
+            print 'Will check authentication again in ' + str(heartbeat_seconds) + ' seconds.'
+            time.sleep(heartbeat_seconds)
+            continue
+        if result == LoginResult.NETWORK_FAILURE:
+            print 'Will retry authentication in ' + str(retry_seconds) + ' seconds.'
+            time.sleep(retry_seconds)
             continue
         else:
             print 'Please try again.'
